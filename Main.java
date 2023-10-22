24ef28b214327150f68543115feb25a234308481
MukulDev <mukulsf70@gmail.com>
2023-10-23 02:05:08 +0530

    Main Java Commit

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main{
     public static void main(String args[]){
-        System.out.println("Hello World");
+        System.out.print("\"Sum of three numbers is\" : ");
+
+        int num1 = 10;
+        int num2 = 2;
+        int num3 = 3;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+
     }
 }
